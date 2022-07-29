a3591ef76bc931935447bd4a4e979698812f8092
Akmal Ismoilov <master80098@gmail.com>
2022-07-29 12:01:59 +0500

    Task completed!

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        if(printToInclusive >= 1)
+            System.out.println("1\n1");
+        else if (printToInclusive == 0)
+            System.out.println("1");
+        else return;
+
+        int factorial = 1;
+        for (int i = 2; i <= printToInclusive; i++)
+        {
+            factorial *= i;
+            System.out.println(factorial);
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        if(power >= 0)
+            System.out.println(1);
+        else
+            System.out.println("too much power");
 
+        int powered = 1;
+        for (int i = 1; i <= power; i++)
+        {
+            powered *= 2;
+            System.out.println(powered);
+        }
     }
 }
